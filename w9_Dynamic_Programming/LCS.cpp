a6f33aa502e665f9c76b03fd4d7ac6af44144c8e
Hong0206 <hongyd20042@gmail.com>
2025-11-30 19:50:01 +0900

    LCS 문제
    DP로 문제 해결, look-up 테이블을 이용하여 중복 하위 문제를 두 번 이상 계산하지 않는 것이 중요

---
+++ w9_Dynamic_Programming/LCS.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// DP로 LCS 구하는 함수
+void LCS(vector<vector<int>> &table, string &x, string &y);
+
+int main(void) {
+
+    // 입출력 고속화
+    ios::sync_with_stdio(false), cin.tie(NULL);
+
+    // pdf 예시 입력
+    string x = "ABCBDAB";
+    string y = "BDCABA";
+
+    // look-up table 선언
+    vector<vector<int>> table(x.length() + 1, vector<int>(y.length() + 1));
+    // 함수 호출하여, LCS 구하기
+    LCS(table, x, y);
+
+    // 답안 출력
+    cout << table[x.length()][y.length()] << '\n';
+
+
+    // 예상 출력: 4
+    return 0;
+}
+
+
+void LCS(vector<vector<int>> &table, string &x, string &y) {
+
+    // 0번 인덱스의 경우, 두 문자열의 길이가 0인 경우 0이므로, 배제
+    for (int i = 1; i <= x.length(); i++) {
+        for (int j = 1; j <= y.length(); j++) {
+            // 문자가 같다면, 이전 (대각선 위) 길이 불러오기
+            if (x[i - 1] == y[j - 1]) table[i][j] = table[i - 1][j - 1] + 1;
+            // 문자가 다르다면, 이전 길이에서 가장 긴 길이로 불러오기
+            else table[i][j] = max(table[i][j - 1], table[i - 1][j]);
+        }
+    }
+}
