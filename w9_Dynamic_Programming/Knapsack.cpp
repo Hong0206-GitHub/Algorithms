5a4174a3b2f05b596c205ece0eae2defa67ac779
Hong0206 <hongyd20042@gmail.com>
2025-12-08 04:28:26 +0900

    냅색 문제 다시 재작성
    초기 코드는 가독성이 너무 떨어지고, 자료형을 너무 복잡하게 사용하여 코드 이해가 어려웠음
    
    따라서 자료형을 여러 개 사용하되 이해하기 쉽게, 가독성 좋게 다시 재작성
    
    또한 백트래킹 함수를 추가하여, 어떤 아이템이 선택되었는지도 확인 할 수 있게 함.

--- w9_Dynamic_Programming/Knapsack.cpp
+++ w9_Dynamic_Programming/Knapsack.cpp
@@ -2,53 +2,86 @@
 
 using namespace std;
 
-// 냅색 알고리즘 DP로 구현
-void knapsack(vector<vector<int>> &table, vector<pair<int, int>> &v);
+// 함수 선언
+void knapsack(int N, int W);
+void print_backtracking(int N, int W);
 
-// 아이템 개수와 가능 무게
-int N, W;
+// 기본 자료형 선언
+vector<int> value;
+vector<int> weight;
+vector<vector<int>> table;
+vector<vector<bool>> keep;
 
 int main(void) {
 
     // 입출력 고속화
     ios::sync_with_stdio(false), cin.tie(NULL);
 
-    // 4개의 아이템과 최대 허용 가능 무게 10
-    N = 4, W = 10;
+    // pdf 예제 값 입력
+    int N = 4, W = 10;
+
+    // 자료형 초기화
+    value.resize(N + 1);
+    weight.resize(N + 1);
+    table.resize(N + 1, vector<int>(W + 1));
+    keep.resize(N + 1, vector<bool>(W + 1, false));
+
+
+    value[1] = 10;
+    value[2] = 40;
+    value[3] = 30;
+    value[4] = 50;
     
-    // look-up table 선언
-    vector<vector<int>> table(N + 1, vector<int>(W + 1));
-    
-    // <가치, 무게> 저장 벡터 선언 (개수 N)
-    vector<pair<int, int>> v;
+    weight[1] = 5;
+    weight[2] = 4;
+    weight[3] = 6;
+    weight[4] = 3;
 
-    // pdf 에서 제공된 예제 입력 값
-    v.push_back({0, 0});
-    v.push_back({10, 5});
-    v.push_back({40, 4});
-    v.push_back({30, 6});
-    v.push_back({50, 3});
+    // 초기값 설정
+    for (int i = 0; i <= W; i++) table[0][i] = 0;
 
-    // 함수 호출하여 look-up table 채우기
-    knapsack(table, v);
-
-    // 가장 마지막 원소가 답임
+    // 함수호출 및 답안 출력
+    knapsack(N, W);
     cout << table[N][W] << '\n';
+    print_backtracking(N, W);
+    cout << '\n';
 
     return 0;
 }
 
-// 함수
-void knapsack(vector<vector<int>> &table, vector<pair<int, int>> &v) {
-    
-    // 아이템들에 대해서
+// DP 알고리즘으로 구현한 냅색 함수
+void knapsack(int N, int W) {
     for (int i = 1; i <= N; i++) {
-        // 무게가 0~ 최대 허용 무게까지 
-        for (int w = 0; w <= W; w++) {
-            // 해당 아이템이 들어갈 수 있는 무게라면,
-            // 해당 아이템을 포함했을 경우와 포함하지 않았을 경우의 최댓값을 선택해 넣음
-            if (v[i].second <= w) table[i][w] = max(table[i - 1][w], v[i].first + table[i - 1][w - v[i].second]);
-            else table[i][w] = table[i - 1][w]; // 넣을 수 없으면 이전 값 그대로 넣음
+        for (int j = 0; j <= W; j++) {
+            // 선택할 수 있는 경우
+            if (weight[i] <= j) {
+                // 해당 아이템을 선택한 경우와 선택하지 않은 경우 중 가치가 큰 값을 선택
+                table[i][j] = max(table[i - 1][j], value[i] + table[i - 1][j - weight[i]]);
+
+                // 선택한 것을 keep 벡터에 기록
+                if (table[i - 1][j] <= value[i] + table[i - 1][j - weight[i]]) keep[i][j] = true;          
+                else keep[i][j] = false;      
+            }
+            else {
+                // 선택할 수 없는 경우는 이전 값 그대로 불러옴
+                table[i][j] = table[i - 1][j]; 
+                keep[i][j] = false;
+            }
+        }
+    }
+}
+
+// 선택한 아이템 인덱스 출력하는 함수
+void print_backtracking(int N, int W) {
+
+    // DP는 하위에서 상위로 올라왔기 때문에
+    // 역추적은 상위에서 하위로 추적
+    for (int i = N; i >= 1; i--) {
+        // 기록 되어있는 경우 출력해주고,
+        if (keep[i][W]) {
+            cout << i << ' ';
+            // 무게 갱신해줌
+            W = W - weight[i];
         }
     }
 }
