c6b6ef3b9a5776a7a3bdd8228f3c57e1a3c32c51
Hong0206 <hongyd20042@gmail.com>
2025-12-08 02:34:28 +0900

    반복문 방식 구현 수정 및 3변수 구현 추가
    반복문 방식에서 초기값을 잘못 설정하여 오류가 있었음을 파악하여 수정함.
    
    또한 더 최적화 가능한 3변수 방식을 추가함

--- w9_Dynamic_Programming/Ways_to_Climb.cpp
+++ w9_Dynamic_Programming/Ways_to_Climb.cpp
@@ -1,39 +1,79 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
+#include <bits/stdc++.h>
 
 using namespace std;
 
 // 재귀방식으로 구현
 int ways_top_down(int n, vector<int> &v) {
+    // 기저 조건
+    // n이 0인 경우는 아무것도 안하는 1가지의 경우의 수만 존재함.
+    // n이 음수인 경우는 경우의 수가 존재하지 않음.
     if (n == 0) return 1;
     if (n < 0) return 0;
 
+    // dp 테이블에 저장된 값이 존재하는 경우 그 값을 사용
+    // 중복 연산 회피 -> DP의 본질
     if (v[n] != -1) return v[n];
 
+    // 위에서 걸러지지 않은 경우,
+    // 연산이 필요함
+
+    // 연산해서 이를 벡터에 저장하여 중복 연산을 피함
     v[n] = ways_top_down(n - 1, v) + ways_top_down(n - 2, v) + ways_top_down(n - 3, v);
+    
+    // 저장한 값을 리턴
     return v[n];
 }
 
 // 반복문 방식으로 구현
 int ways_bottom_up(int n) {
     
+    // 기저 조건
     if (n < 0) return 0;
     if (n == 0) return 1;
 
+    // dp 테이블 선언 (벡터의 크기는 n + 1이어야 함. 이유는 모든 하위문제(본 문제 포함)를 테이블에 포함해야 하기 때문)
     vector<int> dp(n + 1, -1);
-    dp[0] = 0;
+    
+    // 초기값 삽입
+    dp[0] = 1;
+
+    // 범위 벗어나지 않게 범위 조절 해주고, dp 반복문엔 점화식 하나만 넣어주기 위해서
+    // 초기값 추가 삽입
     if (n >= 1) dp[1] = 1;
     if (n >= 2) dp[2] = 2;
 
+    // 점화식
     for (int i = 3; i <= n; i++) {
-        dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
+        dp[i] = dp[i - 3] + dp[i - 2] + dp[i - 1];
     }
 
     return dp[n];
-
 }
 
+// 3변수를 사용하여 구현 (반복문 보다 공간 복잡도가 적음)
+int ways_bottom_up_clear(int n) {
+
+    // 기저 조건
+    if (n < 0) return 0;
+    if (n == 0) return 1;
+
+    // dp 테이블 사용 대신, 연산 루틴을 고려하여 3개의 변수만 사용
+    int temp1 = 1, temp2 = 2, temp3 = 4, answer;
+
+    // for문에 부합하지 않는 n의 값을 미리 처리
+    if (n == 1) return temp1;
+    if (n == 2) return temp2;
+
+    // 점화식
+    for (int i = 3; i <= n; i++) {
+        answer = temp3 + temp2 + temp1;
+        temp1 = temp2;
+        temp2 = temp3;
+        temp3 = answer;
+    }
+
+    return answer;
+}
 
 int main(void) {
 
